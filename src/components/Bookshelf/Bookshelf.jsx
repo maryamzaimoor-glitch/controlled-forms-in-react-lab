2ac0dd02c27f324a469db9cb6f9d11bc8dd1ab79
Mariam Alzaimoor <maryamzaimoor@gmail.com>
2026-02-09 00:42:31 +0300

    first commit

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+  const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' }
+  ]);
+
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setNewBook({
+      ...newBook,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setBooks([...books, newBook]);
+
+    setNewBook({
+      title: '',
+      author: ''
+    });
+  };
+
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+        {/* Form will go here */}
+        <div className="formDiv">
+        <h3>Add a Book</h3>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>
+            Author:
+            <input
+              type="text"
+              name="author"
+              value={newBook.author}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+      </div>
+
+      <div className="bookCardsDiv">
+        {/* Book cards will display here */}
+              <div className="bookCardsDiv">
+        {books.map((book, index) => (
+          <div key={index} className="bookCard">
+            <h4>{book.title}</h4>
+            <p>by {book.author}</p>
+          </div>
+        ))}
+      </div>
+        </div>  
+    );  
+};
+export default Bookshelf;
